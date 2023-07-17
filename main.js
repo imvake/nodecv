398876a39bc7bde4e925b19524cd05151a240c73
imvake <104240205+imvake@users.noreply.github.com>
2023-07-18 01:22:29 +0200

    my cv using nodemailer for the contact form

---
+++ main.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer");
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.use("/assets", express.static(path.join(__dirname, "views", "assets")));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+console.log(process.env.AUTH_PASS);
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Create a Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "altarteelbot@gmail.com",
+      pass: "uphewvzfmgucqqbr",
+    },
+  });
+
+  // Configure the email options
+  const mailOptions = {
+    from: email,
+    to: "al8hm92@gmail.com",
+    subject: "New Contact Form Submission",
+    text: `
+        Name: ${name}
+        Email: ${email}
+        Message: ${message}
+      `,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.json({ success: false });
+    } else {
+      console.log("Email sent:", info.response);
+      res.json({ success: true });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
